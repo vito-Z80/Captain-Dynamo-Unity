8d6594c76800357544ff9c743d4eeb505e4d6936
Виталий <efiop80@gmail.com>
2023-10-19 23:38:02 +0700

    bumper chaged. Add on/off sprite.

--- Assets/Scripts/Game/Platforms/Bumper.cs
+++ Assets/Scripts/Game/Platforms/Bumper.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Game.Platforms
 {
     public class Bumper : MonoBehaviour
     {
+        public Sprite bumperOffSprite;
+        public Sprite bumperOnSprite;
         public float bounceForce = 100f;
+
+        private SpriteRenderer _spriteRenderer;
+
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            SetSprite(bumperOffSprite);
+        }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
@@ -17,11 +28,18 @@ namespace Game.Platforms
 
         private IEnumerator ActiveState(Collision2D collision)
         {
+            SetSprite(bumperOnSprite);
             var hc = collision.gameObject.GetComponent<HeroController>();
             hc.isActive = false;
             yield return new WaitForSeconds(0.2f);
             hc.isActive = true;
+            SetSprite(bumperOffSprite);
             yield return null;
+        }
+
+        private void SetSprite(Sprite sprite)
+        {
+            _spriteRenderer.sprite = sprite;
         }
     }
 }
